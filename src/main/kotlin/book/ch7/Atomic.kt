1c1468087910f73a23e1453b374179345f467c03
mrbluesky <gogetit88@gmail.com>
2022-11-25 08:03:48 +0900

    actor extension

--- src/main/kotlin/book/ch7/Atomic.kt
+++ src/main/kotlin/book/ch7/Atomic.kt
@@ -59,30 +59,90 @@ import kotlinx.coroutines.channels.actor
  * 액터의 역할
  * 액터는 상태 엑세스를 단일 스레드로 한정하고, 다른 스레드가 채널로 상태 수정을 요청할 수 있다.
  */
-private var counter = 0
-private val context = newSingleThreadContext("counter")
-fun getCounter() = counter
-val actorCounter = GlobalScope.actor<Void?> {
-    for(msg in channel) {
-        counter++
-    }
-}
-fun asyncIncrement(by: Int, name: String) = GlobalScope.async {
-    for(i in 0 until by) {
-        println("${name}|${Thread.currentThread().name} : $i")
-        actorCounter.send(null)
-    }
-}
-fun main(args: Array<String>) = runBlocking {
-    val workerA = asyncIncrement(200, "A")
-    val workerB = asyncIncrement(1500, "B")
-
-    workerA.await()
-    workerB.await()
-
-    println("counter [${getCounter()}]")
-}
+//private var counter = 0
+//private val context = newSingleThreadContext("counter")
+//fun getCounter() = counter
+//val actorCounter = GlobalScope.actor<Void?> {
+//    for(msg in channel) {
+//        counter++
+//    }
+//}
+//fun asyncIncrement(by: Int, name: String) = GlobalScope.async {
+//    for(i in 0 until by) {
+//        println("${name}|${Thread.currentThread().name} : $i")
+//        actorCounter.send(null)
+//    }
+//}
+//fun main(args: Array<String>) = runBlocking {
+//    val workerA = asyncIncrement(200, "A")
+//    val workerB = asyncIncrement(1500, "B")
+//
+//    workerA.await()
+//    workerB.await()
+//
+//    println("counter [${getCounter()}]")
+//}
 
 /**
  * 액터를 사용한 기능 확장
+ * 액터를 사용 하면 채널이 전체 코루틴을 원자적으로 유지하면서 더 높은 유연성을 허용할 수 있다.
+ * 액터를 사용하여 카운터를 늘리거나 줄일 수 있도록 만들어보자
+ */
+
+//var counter = 0
+//enum class Action {
+//    INCREASE, DECREASE
+//}
+//
+//val context = newFixedThreadPoolContext(20, "threadPool")
+//var actorCounter = GlobalScope.actor<Action>(context) {
+//    for(msg in channel) {
+//        when(msg) {
+//            Action.DECREASE -> {
+//                println("[${Thread.currentThread().name}] $msg")
+//                counter--
+//            }
+//            Action.INCREASE -> {
+//                println("[${Thread.currentThread().name}] $msg")
+//                counter++
+//            }
+//        }
+//    }
+//}
+//
+//fun asyncDecrement(by: Int) = GlobalScope.async {
+//    for(i in 0 until by) {
+//        actorCounter.send(Action.DECREASE)
+////        counter--
+//    }
+//}
+//
+//fun asyncIncrement(by: Int) = GlobalScope.async {
+//    for(i in 0 until by) {
+//        actorCounter.send(Action.INCREASE)
+////        counter++
+//    }
+//}
+//
+//fun main(args: Array<String>) = runBlocking {
+//    /*
+//     * 메인 스레드는 CommonPool을 사용하여 카운터를 동시에 증가 및 감소시키는 코루틴을 생성하며,
+//     * 코루틴은 액션과 함께 메시지를 액터에 보내서 이를 수행한다.
+//     * 액터는 특정 스레드에 한정된 코루틴이기 때문에 모든 수정은 원자적
+//     */
+//    val workerA = asyncIncrement(2000)
+//    val workerB = asyncIncrement(100)
+//    val workerC = asyncDecrement(1000)
+//
+//    workerA.await()
+//    workerB.await()
+//    workerC.await()
+//    println("counter $counter")
+//
+//}
+
+/**
+ * 버퍼드 액터
+ * 액터는 다른 송신채널과 동일하게 버퍼링될 수 있음
+ * 기본적으로 모든 액터는 버퍼링 되지 않음. 메시지가 수신될 때까지 발신자를 send()에서 일시중단함
  */
